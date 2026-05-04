79df6e3ef966329952255fe9eb13da92025c25ff
Alia Lescoulie <alia.lescoulie@gmail.com>
2026-05-04 14:21:50 -0700

    adds meta proof info

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -21,26 +21,35 @@ data Proof
   | App Proof Proof
   deriving (Show, Eq)
 
+data MetaProof
+  = ID Proof
+  | ANDR MetaProof MetaProof
+  | FUNR String MetaProof
+  | ANDL1 Proof MetaProof
+  | ANDL2 Proof MetaProof
+  | FUNL Proof MetaProof MetaProof
+  deriving (Show, Eq)
+
 type Ctx = [(Proof, Prop)]
 
-idRule :: Ctx -> Prop -> [Proof]
-idRule g p = [m | (m, p') <- g, p' == p]
+idRule :: Ctx -> Prop -> [MetaProof]
+idRule g p = [ID m | (m, p') <- g, p' == p]
 
-andRight :: Ctx -> Prop -> [Proof]
+andRight :: Ctx -> Prop -> [MetaProof]
 andRight g (Prod a b) = do
   m <- prove g a
   n <- prove g b
-  return $ Pair m n
+  return $ ANDR m n
 andRight _ _ = []
 
-funRight :: Ctx -> Prop -> [Proof]
+funRight :: Ctx -> Prop -> [MetaProof]
 funRight g (Fun a b) = do
   let x = "x" ++ show (length g)
   m <- prove ((Var x,a):g) b
-  return $ Lam x m
+  return $ FUNR x m
 funRight _ _ = []
 
-rightRules :: Ctx -> Prop -> [Proof]
+rightRules :: Ctx -> Prop -> [MetaProof]
 rightRules g a =
       andRight g a
   <|> funRight g a
@@ -57,55 +66,66 @@ isProd :: Prop -> Bool
 isProd (Prod _ _) = True
 isProd _ = False
 
-andLeft1 :: Ctx -> Prop -> [Proof]
+andLeft1 :: Ctx -> Prop -> [MetaProof]
 andLeft1 g c = 
   case grab (isProd.snd) g of
     Just ((m, Prod a b), g') -> do
-      prove ((Fst m, a):g') c
+      d <- prove ((Fst m, a):g') c
+      return $ ANDL1 m d
     _ -> []
 
-andLeft2 :: Ctx -> Prop -> [Proof]
+andLeft2 :: Ctx -> Prop -> [MetaProof]
 andLeft2 g c =
   case grab (isProd.snd) g of
     Just ((m, Prod a b), g') -> do
-      prove ((Snd m, b):g') c
+      d <- prove ((Snd m, b):g') c
+      return $ ANDL2 m d
     _ -> []
 
 isFun :: Prop -> Bool
 isFun (Fun _ _) = True
 isFun _ = False
 
-funLeft :: Ctx -> Prop -> [Proof]
+funLeft :: Ctx -> Prop -> [MetaProof]
 funLeft g c =
   case grab (isFun.snd) g of
     Just ((m, Fun a b), g') -> do
-      n <- prove g' a
-      prove ((App m n, b) : g') c
+      d <- prove g' a
+      e <- prove ((App m (extractProof d), b) : g') c
+      return $ FUNL m d e
     _ -> []
 
-leftRules :: Ctx -> Prop -> [Proof]
+leftRules :: Ctx -> Prop -> [MetaProof]
 leftRules g p =
       andLeft1 g p
   <|> andLeft2 g p
   <|> funLeft g p
 
-prove :: Ctx -> Prop -> [Proof]
+prove :: Ctx -> Prop -> [MetaProof]
 prove g p =
   idRule g p <|> rightRules g p <|> leftRules g p
 
+extractProof :: MetaProof -> Proof
+extractProof (ID m) = m
+extractProof (ANDR d e) = Pair (extractProof d) (extractProof e)
+extractProof (FUNR x d) = Lam x (extractProof d)
+extractProof (ANDL1 _ d) = extractProof d
+extractProof (ANDL2 _ d) = extractProof d
+extractProof (FUNL _ _ e) = extractProof e
+
 main :: IO ()
 main = do
-  print $ prove [] A
-  print $ prove [(Var "x", A)] A
-  print $ prove [(Var "x", A), (Var "y", B)] (Prod A B)
-  print $ prove [(Var "p", Prod A B)] B
-  print $ prove [(Var "p", Prod A B)] (Prod B A)
-  print $ prove [] (Fun A A)
-  print $ prove [(Var "x", B)] (Fun A B)
-  print $ prove [(Var "f", Fun A B), (Var "x", A)] B
-  print $ prove [] (Fun A (Fun (Fun A B) B))
-  print $ prove [(Var "x", Prod A (Prod B C))] (Prod (Prod A B) C)
-  print $ prove [(Var "f", Fun (Prod A B) C)] (Fun A (Fun B C))
-  print $ prove [(Var "x", A), (Var "y", A)] A
-  print $ prove [(Var "x", Prod A B), (Var "y", Fun B A)] A
+--  print $ prove [] A
+--  print $ prove [(Var "x", A)] A
+--  print $ prove [(Var "x", A), (Var "y", B)] (Prod A B)
+--  print $ prove [(Var "p", Prod A B)] B
+--  print $ prove [(Var "p", Prod A B)] (Prod B A)
+--  print $ prove [] (Fun A A)
+--  print $ prove [(Var "x", B)] (Fun A B)
+--  print $ prove [(Var "f", Fun A B), (Var "x", A)] B
+--  print $ prove [] (Fun A (Fun (Fun A B) B))
+--  print $ prove [(Var "x", Prod A (Prod B C))] (Prod (Prod A B) C)
+--  print $ prove [(Var "f", Fun (Prod A B) C)] (Fun A (Fun B C))
+--  mapM_ print $ prove [(Var "x", A), (Var "y", A)] A
+  mapM_ print $ prove [(Var "x", Prod A B), (Var "y", Fun B A)] A
 
